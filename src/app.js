6b6ba5478bfe3962fc7e5c06548effc2096ca7eb
MiguelGontijo <mpgontijo84@gmail.com>
2021-02-13 16:24:31 -0300

    densenvolvimento desafio coneitos

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { v4: uuid, validate: isUuid, v4 } = require('uuid');
+const { v4: uuid, validate: isUuid, v4, validate } = require('uuid');
 
 const app = express();
 
@@ -10,8 +10,38 @@ app.use(cors());
 
 const repositories = [];
 
+function vldRepositoriesID (req, res, next){
+const { id } = req.params;
+
+  if (!validate(id)){
+    return res.status(400).json({error: "Ivalid Repositories ID"});
+  }
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0){
+    return res.status(400).json( {error: "Repositories ID not found!"} );
+  }
+  
+  return next();
+}
+
+app.use("/repositories/:id",vldRepositoriesID);
+
 app.get("/repositories", (request, response) => {
-  return response.status(200).json( repositories );
+  const { title, url, techs } = request.query;
+  
+  let results = title
+    ? repositories.filter(repo => repo.title.includes(title) )
+    : repositories 
+  /* results = url
+    ? repositories.filter(repo => repo.url.includes(url) )
+    : repositories 
+  results = techs
+    ? repositories.filter(repo => repo.techs.includes(techs) )
+    : repositories */
+
+  return response.status(200).json( results );
 });
 
 app.post("/repositories", (request, response) => {
@@ -39,9 +69,11 @@ app.put("/repositories/:id", (request, response) => {
   if (repoIndex < 0){
     return response.status(400).json( {error:"Repositorie ID not found!"} );
   }
-  const repo = { title,
+  const repo = { id,
+                  title,
                   url,
                   techs,
+                  likes: repositories[repoIndex].likes
                 };
 
   repositories[repoIndex] = repo 
@@ -59,12 +91,12 @@ app.delete("/repositories/:id", (request, response) => {
     return response.status(400).json( {error: "Repositories ID not found!"} );
   }
 
-  repositories.slice(repoIndex,1);
+  repositories.splice(repoIndex,1);
 
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like",vldRepositoriesID, (request, response) => {
   const { id } = request.params;
 
   const repoIndex = repositories.findIndex(repo => repo.id === id);
